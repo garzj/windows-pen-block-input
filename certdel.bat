16cb2c373be6907ca54ac29182412f6c80c49df3
garzj <jojogarz123@gmail.com>
2022-03-12 15:40:37 +0100

    Only delete existing certificates

--- certdel.bat
+++ certdel.bat
@@ -3,8 +3,10 @@
 set store=%1
 set file=%2
 
-for /F "tokens=3" %%s in ('certutil -dump %file% ^| findstr ^"^^Serial^"') do (
-   certutil -user -delstore %store% %%s
+if exist %file% (
+   for /F "tokens=3" %%s in ('certutil -dump %file% ^| findstr ^"^^Serial^"') do (
+      certutil -user -delstore %store% %%s
+   )
 )
 
 @echo on
