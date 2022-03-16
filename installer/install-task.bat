5da588410318577d2d492fb1eddcf51a03f75ea9
garzj <jojogarz123@gmail.com>
2022-03-16 15:09:19 +0100

    Run task as adminsitrator group

--- installer/install-task.bat
+++ installer/install-task.bat
@@ -2,6 +2,6 @@ powershell -c ^
   $action = New-ScheduledTaskAction -Execute '%1'; ^
   $trigger = New-ScheduledTaskTrigger -AtLogon -RandomDelay 00:00:15; ^
   $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Compatibility Win8; ^
-  $principal = New-ScheduledTaskPrincipal -UserId '%username%' -RunLevel Highest; ^
+  $principal = New-ScheduledTaskPrincipal -GroupId 'BUILTIN\Administrators' -RunLevel Highest; ^
   $definition = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings -Description 'Run Stylus Block Input at startup.'; ^
   Register-ScheduledTask -TaskName 'Stylus Block Input' -InputObject $definition
