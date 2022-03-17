e9e6f4eaa690057a836407c21072828f7d08bb0c
garzj <jojogarz123@gmail.com>
2022-03-17 10:08:33 +0100

    Use the admin group id instead of a localized name

--- installer/install-task.bat
+++ installer/install-task.bat
@@ -2,6 +2,6 @@ powershell -c ^
   $action = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument '/c start """""""" ""%1""'; ^
   $trigger = New-ScheduledTaskTrigger -AtLogon -RandomDelay 00:00:15; ^
   $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Compatibility Win8; ^
-  $principal = New-ScheduledTaskPrincipal -GroupId 'BUILTIN\Administrators' -RunLevel Highest; ^
+  $principal = New-ScheduledTaskPrincipal -GroupId S-1-5-32-544 -RunLevel Highest; ^
   $definition = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings -Description 'Run Stylus Block Input at startup.'; ^
   Register-ScheduledTask -TaskName 'Stylus Block Input' -InputObject $definition
